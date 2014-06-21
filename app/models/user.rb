9facefdb72f0b210d3dbdfdd13b9c5683e77f7ef
Boris <boris.bbk@gmail.com>
2014-06-21 08:11:45 +0300

    fixes VK missing avatars; fixes map hover CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User
   #Additional fields
   field :email, type: String
   field :avatar, type: String
+  field :avatar_medium, type: String
   field :avatar_original, type: String
   field :want_to_be_master, type: Mongoid::Boolean
   field :social_network_link, type: String
@@ -33,13 +34,16 @@ class User
     if user
       user.update_attributes(name: auth_hash[:info][:first_name],
                              avatar: auth_hash[:info][:image],
-                             avatar_original: auth_hash[:extra][:raw_info][:photo_big],
+                             avatar_original: auth_hash[:extra][:raw_info][:photo_200_orig],
+                             avatar_medium: auth_hash[:extra][:raw_info][:photo_100],
                              social_network_link: auth_hash[:info][:urls][:Vkontakte])
       user
     else
       User.create(uid: auth_hash[:uid],
                   name: auth_hash[:info][:first_name],
                   avatar: auth_hash[:info][:image],
+                  avatar_original: auth_hash[:extra][:raw_info][:photo_200_orig],
+                  avatar_medium: auth_hash[:extra][:raw_info][:photo_100],
                   social_network_link: auth_hash[:info][:urls][:Vkontakte])
     end
   end
