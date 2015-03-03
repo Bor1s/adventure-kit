6b9d2e4f8332cb3122e02ef82535a68531f05272
Boris <boris.bbk@gmail.com>
2015-03-03 16:47:05 +0200

    comments out CoreNotificator

--- config/initializers/subscribers.rb
+++ config/initializers/subscribers.rb
@@ -1,20 +1,20 @@
 ActiveSupport::Notifications.subscribe('event_created') do |name, start, finish, id, payload|
   @event = Event.find(payload[:id])
   @event.game.subscribers.each do |subscriber|
-    if subscriber.email.present?
-      CoreNotificator.delay.event_created(@event.id, subscriber.id)
-    end
+    #if subscriber.email.present?
+      #CoreNotificator.delay.event_created(@event.id, subscriber.id)
+    #end
   end
 end
 
 ActiveSupport::Notifications.subscribe('join_game') do |name, start, finish, id, payload|
-  CoreNotificator.delay.join_game(payload)
+  #CoreNotificator.delay.join_game(payload)
 end
 
 ActiveSupport::Notifications.subscribe('left_game') do |name, start, finish, id, payload|
-  CoreNotificator.delay.left_game(payload)
+  #CoreNotificator.delay.left_game(payload)
 end
 
 ActiveSupport::Notifications.subscribe('player_rejected') do |name, start, finish, id, payload|
-  CoreNotificator.delay.player_rejected(payload)
+  #CoreNotificator.delay.player_rejected(payload)
 end
